80c8786a4d73c9ac55a600c90ee4155da2b3c5d0
clbakerx <clbakerx@gmail.com>
2026-03-25 13:04:44 -0700

    Fix singles page build

--- app/singles/page.tsx
+++ app/singles/page.tsx
@@ -301,6 +301,15 @@ const SINGLES: SingleTrack[] = [
     previewUrl:
       "https://filedn.com/ldxHrdHcf3tV7YntUkvw8R0/Singles/Wouldnt-Want-To/Wouldnt-Want-To_Sample.mp3",
   },
+  {
+    id: "48-hours",
+    title: "48 Hours",
+    subtitle: "Song Preview",
+    priceLabel: "$3.99",
+    buyUrl: DEFAULT_BUY_URL,
+    previewUrl:
+      "https://filedn.com/ldxHrdHcf3tV7YntUkvw8R0/Singles/48-Hours/48-Hours_V2_Sample.mp3",
+  },
 ];
 
 export default function SinglesPage() {
