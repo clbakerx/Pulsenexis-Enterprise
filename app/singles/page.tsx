5ffffef6da85fa032f53db3a62fa22d0ac1f6c5b
clbakerx <clbakerx@gmail.com>
2026-03-25 10:01:54 -0700

    update

--- app/singles/page.tsx
+++ app/singles/page.tsx
@@ -299,7 +299,7 @@ const SINGLES: SingleTrack[] = [
     priceLabel: "$3.99",
     buyUrl: DEFAULT_BUY_URL,
     previewUrl:
-      "https://filedn.com/ldxHrdHcf3tV7YntUkvw8R0/Singles/Wouldn’t-Want-To/Wouldnt-Want-To_Sample.mp3",
+      "https://filedn.com/ldxHrdHcf3tV7YntUkvw8R0/Singles/Wouldnt-Want-To/Wouldnt-Want-To_Sample.mp3",
   },
 
 export default function SinglesPage() {
